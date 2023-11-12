53ee096a711ef5ab0a7298bf0b2a3974c090e895
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-11-12 18:28:53 +0100

    implement pagination for contacts display

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -1,11 +1,21 @@
 import Contact from "#models/contact.js";
 
 export const getContacts = async (req, res, next) => {
+  const { page = 1, limit = 20 } = req.query;
+  const startIndex = (page - 1) * limit;
+
   try {
-    const contacts = await Contact.find();
+    const [contacts, totalContacts] = await Promise.all([
+      Contact.find().skip(startIndex).limit(limit),
+      Contact.countDocuments(),
+    ]);
+    const totalPages = Math.ceil(totalContacts / limit);
 
     res.status(200).json({
       data: contacts,
+      total: totalContacts,
+      totalPages,
+      currentPage: page,
     });
   } catch (error) {
     res.status(404).json({
