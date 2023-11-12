b7bea8b892fc5ac72dc506b446012b26e842a70c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-11-12 19:02:11 +0100

    shorten routing imports

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,19 @@
 import express from "express";
 
-import {
-  getContacts,
-  getContactById,
-  postContact,
-  putContact,
-  deleteContact,
-  patchContactStatus,
-} from "#controllers/contacts/index.js";
+import * as controllers from "#controllers/contacts/index.js";
 import { authMiddleware } from "#middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getContacts);
-router.get("/:contactId", authMiddleware, getContactById);
-router.post("/", authMiddleware, postContact);
-router.delete("/:contactId", authMiddleware, deleteContact);
-router.put("/:contactId", authMiddleware, putContact);
-router.patch("/:contactId/favorite", authMiddleware, patchContactStatus);
+router.get("/", authMiddleware, controllers.getContacts);
+router.get("/:contactId", authMiddleware, controllers.getContactById);
+router.post("/", authMiddleware, controllers.postContact);
+router.delete("/:contactId", authMiddleware, controllers.deleteContact);
+router.put("/:contactId", authMiddleware, controllers.putContact);
+router.patch(
+  "/:contactId/favorite",
+  authMiddleware,
+  controllers.patchContactStatus
+);
 
 export { router as contactsRouter };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,14 @@
 import express from "express";
 
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-  currentUser,
-  updateUserSub,
-} from "#controllers/users/index.js";
+import * as controllers from "#controllers/users/index.js";
 import { authMiddleware } from "#middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/signup", registerUser);
-router.post("/login", loginUser);
-router.get("/logout", authMiddleware, logoutUser);
-router.get("/current", authMiddleware, currentUser);
-router.patch("/subscription", authMiddleware, updateUserSub);
+router.post("/signup", controllers.registerUser);
+router.post("/login", controllers.loginUser);
+router.get("/logout", authMiddleware, controllers.logoutUser);
+router.get("/current", authMiddleware, controllers.currentUser);
+router.patch("/subscription", authMiddleware, controllers.updateUserSub);
 
 export { router as usersRouter };
