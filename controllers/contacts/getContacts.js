018e3216387d4b67dc98ea7a903aa050a304da57
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-11-12 18:37:51 +0100

    implement contact filtering by favorite field

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -1,21 +1,25 @@
 import Contact from "#models/contact.js";
 
 export const getContacts = async (req, res, next) => {
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const startIndex = (page - 1) * limit;
 
   try {
+    const setFavorite = favorite ? { favorite: true } : {};
+
     const [contacts, totalContacts] = await Promise.all([
-      Contact.find().skip(startIndex).limit(limit),
-      Contact.countDocuments(),
+      Contact.find(setFavorite).skip(startIndex).limit(limit),
+      Contact.countDocuments(setFavorite),
     ]);
+
     const totalPages = Math.ceil(totalContacts / limit);
+    const currentPage = parseInt(page);
 
     res.status(200).json({
       data: contacts,
       total: totalContacts,
       totalPages,
-      currentPage: page,
+      currentPage,
     });
   } catch (error) {
     res.status(404).json({
