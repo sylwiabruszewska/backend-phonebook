04d206514dc19cb17ac7086b0c35dbe2bc234ab4
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-11-15 00:33:36 +0100

    add upload middleware

--- middleware/index.js
+++ middleware/index.js
@@ -3,3 +3,5 @@ export * from "./loggerHandler.js";
 export * from "./corsHandler.js";
 export * from "./bodyParserHandler.js";
 export * from "./passportHandler.js";
+export * from "./authMiddleware.js";
+export * from "./uploadMiddleware.js";
