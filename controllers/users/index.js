df0a358424f765aaccc227850e16d0c7fc10d113
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-11-15 20:37:13 +0100

    refactor current user request handler

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,5 +1,5 @@
 export * from "./registerUser.js";
 export * from "./loginUser.js";
 export * from "./logoutUser.js";
-export * from "./currentUser.js";
+export * from "./getCurrentUser.js";
 export * from "./updateUserSub.js";
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 import express from "express";
 
 import * as controllers from "#controllers/users/index.js";
-import { authMiddleware } from "#middleware/authMiddleware.js";
+import { authMiddleware } from "#middleware/index.js";
 
 const router = express.Router();
 
 router.post("/signup", controllers.registerUser);
 router.post("/login", controllers.loginUser);
 router.get("/logout", authMiddleware, controllers.logoutUser);
-router.get("/current", authMiddleware, controllers.currentUser);
+router.get("/current", authMiddleware, controllers.getCurrentUser);
 router.patch("/subscription", authMiddleware, controllers.updateUserSub);
 
 export { router as usersRouter };
