9310b6cf075b01dfa38ee799c193f4f8bc81fbd9
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-03 12:28:35 +0200

    improve performance by removing unnecessary database query

--- controllers/users/getCurrentUser.js
+++ controllers/users/getCurrentUser.js
@@ -1,17 +1,9 @@
-import User from "#models/user.js";
+export const getCurrentUser = async (req, res) => {
+  const { name, email, subscription, avatarURL } = req.user;
 
-export const getCurrentUser = async (req, res, next) => {
-  const user = req.user;
-
-  try {
-    const currentUser = await User.findById(user.id);
-
-    res.json({
-      email: user.email,
-      subscription: currentUser.subscription,
-      avatarURL: currentUser.avatarURL,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({
+    status: "Success",
+    code: 200,
+    data: { name, email, subscription, avatarURL },
+  });
 };
