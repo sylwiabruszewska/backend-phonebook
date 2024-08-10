980467b888bd9ffcb5588cb4ca3fbff22d8f50cb
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-10 19:05:33 +0200

    remove unnecessary import

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,7 +2,6 @@ export * from "./registerUser.js";
 export * from "./loginUser.js";
 export * from "./logoutUser.js";
 export * from "./getCurrentUser.js";
-export * from "./updateUserSub.js";
 export * from "./updateAvatar.js";
 export * from "./verifyUser.js";
 export * from "./resendVerificationMail.js";
