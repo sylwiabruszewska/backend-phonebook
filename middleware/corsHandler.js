f8f89e3b1509ba19b53c36031a1cd1b9d448a3b6
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-10 18:58:44 +0200

    update cross origin

--- middleware/corsHandler.js
+++ middleware/corsHandler.js
@@ -1,5 +1,14 @@
 import cors from "cors";
 
+const corsOrigin = process.env.CORS_ORIGIN || process.env.CORS_ORIGIN_LOCAL;
+
 export const corsHandler = (app) => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: corsOrigin,
+      credentials: true,
+      allowedHeaders: ["Authorization", "Content-Type"],
+      exposedHeaders: ["Authorization"],
+    })
+  );
 };
