367476a05daf0254dbb4b4965b4f8ce55c2a9145
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-11 10:28:17 +0200

    update cors methods

--- middleware/corsHandler.js
+++ middleware/corsHandler.js
@@ -7,7 +7,7 @@ export const corsHandler = (app) => {
     cors({
       origin: "*",
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Authorization", "Content-Type"],
       exposedHeaders: ["Authorization"],
     })
